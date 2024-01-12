1518e96111e4b7b47aaf338276a1f5ed4deb3b72
Luke Allan <thisislukeallan@gmail.com>
2024-01-11 23:47:43 -0600

    added docstrings to functions

--- Flask/flask-greet-calc/calc/app.py
+++ Flask/flask-greet-calc/calc/app.py
@@ -5,24 +5,28 @@ app = Flask(__name__)
 
 @app.route('/add')
 def m_add():
+    """Take in a and b from URL and add"""
     a = int(request.args['a'])
     b = int(request.args['b'])
     return str(add(a, b))
 
 @app.route('/sub')
 def m_sub():
+    """Take in a and b form URL and subtract"""
     a = int(request.args['a'])
     b = int(request.args['b'])
     return str(sub(a, b))
 
 @app.route('/mult')
 def m_mult():
+    """Take in a and b from URL and multiply"""
     a = int(request.args['a'])
     b = int(request.args['b'])
     return str(mult(a, b))
 
 @app.route('/div')
 def m_div():
+    """Take in a and b from URL and divide"""
     a = int(request.args['a'])
     b = int(request.args['b'])
     return str(div(a, b))
@@ -36,6 +40,7 @@ operations = {
 
 @app.route('/math/<eq>')
 def math(eq):
+    """Take in a and b from URL and do a range of math operations: add, sub, mult, div"""
     a = int(request.args['a'])
     b = int(request.args['b'])
     return str(operations[eq](a, b))
--- Flask/flask-greet-calc/greet/app.py
+++ Flask/flask-greet-calc/greet/app.py
@@ -4,12 +4,15 @@ app = Flask(__name__)
 
 @app.route('/welcome')
 def welcome():
+    """Welcomes user"""
     return "welcome"
 
 @app.route('/welcome/home')
 def welcome_home():
+    """Welcomes user home"""
     return "welcome home"
 
 @app.route('/welcome/back')
 def welcome_back():
+    """Welcomes user back"""
     return "welcome back"
